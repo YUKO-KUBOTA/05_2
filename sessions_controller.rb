ace54e38c07ed805ccd88cf7792fa1058941e2e6
KUBOTA <kuro_bouz@icloud.com>
2019-08-04 03:53:06 +0000

    rails05_task

---
+++ sessions_controller.rb
@@ -0,0 +1,49 @@
+# class SessionsController < ApplicationController
+#   def new
+#   end
+# end
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # user = User.find_by(email: params[:session][:email])
+    # if user && user.authenticate(params[:session][:password])
+    
+    user = User.find_by(email: email_params[:email])
+    if user && user.authenticate(password_params[:password])
+#上記Rails 05　課題:SessionsControllerにストロングパラメーターを実装
+
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+
+  def email_params
+    params.require(:session).permit(:email)
+  end
+
+  def password_params
+    params.require(:session).permit(:password)
+  end
+#上記Rails 05　課題:SessionsControllerにストロングパラメーターを実装
+
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
